32ed0a10cea2c3f54c5ad9922a6295f37fd6c480
saadahmed19914 <64952128+saadahmed19914@users.noreply.github.com>
2020-06-14 15:49:59 +0500

    Add files via upload

---
+++ saad js/app.js
@@ -0,0 +1,269 @@
+//               === Assignment No:1 ===
+
+// Q:1 
+
+// alert('Welcome to Java Script Website');
+
+// Q:2 
+
+// alert('Error! Please enter a valid password');
+
+// Q:3
+
+// alert('Welcome to JS Land...\nHappy Coding');
+
+// Q;4
+
+// alert('Welcome to JS Land...');
+// alert('Happy coding!\nPrevent this page from creating additional dialogs. ');
+
+// Q:5
+
+// console.log('Hello... I can run JS through my web browser`s console');
+
+
+//              === Assignment No:2 ===
+
+// Q:1 
+
+// var username = 'Ayan Ahmed';
+
+// Q:2
+
+// var myname = 'Saad Ahmed';
+
+// Q:3
+
+// var titled = 'Hello World';
+// alert(titled);
+
+// Q:4
+
+// var stuName = 'Saadi';
+// alert(stuName);
+
+// var stuage = '22 year old';
+// alert(stuage);
+
+// var courseName = 'Certified App Development';
+// alert(courseName);
+
+// Q:5
+
+// var pizza = 'PIZZA \n PIZZ \n PIZ \n PI \n P';
+// alert(pizza);
+
+// Q:6 
+
+// var addrs = 'My email address is example@example.com';
+// alert(addr);
+
+// Q:7
+
+// var display = 'A smarter way to learn JavaScript';
+// alert(display);
+
+// Q:8
+
+// document.write('Yah! I can write HTML content through Javascript');
+
+// Q:9
+
+// var design = " ==================@&o&@====================";
+// alert(design);
+
+
+//                  ==== Assigment No:3 ====
+
+// Q:1
+
+// var age = "I am 22 years old ";
+// alert(age);
+
+// Q:2
+
+// var time = 'You have visited this site 18 times';
+// alert(time);
+
+// Q:3
+
+// var birth = 'My birth year is 1994 <br/> Data type of my declared variable is number';
+// document.write(birth);
+
+// Q:4
+
+// var name = ' <b> johny </b> ordered <b> 5 T-shirt </b>(s) on XYZ Cloting store';
+// document.write(name);
+
+//                    ==== Assigment No:4 ====
+
+
+// Q:1
+
+// var i = 'First Statement ';
+// var j = 'Second Statement ';
+// var k = 'Third Statement ';
+// var p = i + j + k;
+
+// Q:2
+
+// var legal1 = 'Legal 1';
+// var legal2 = 'Legal 2';
+// var legal3 = 'Legal 3';
+// var legal4 = 'Legal 4';
+// var legal5 = 'Legal 5';
+
+// var @illegal = 'Illegal 1';
+// var #illegal = 'Illegal 2';
+// var 1illegal = 'Illegal 3';
+// var 2illegal = 'Illegal 4';
+// var 3illegal = 'Illegal 5';
+
+// Q:3
+
+// var heading = '<h1>A heading stating "Rules for naming JS variables"</h1>';
+// document.write(heading + "<br>");
+
+// var b = 'Variable names can only contain numbers , $ , For example : $my_1stVariable ';
+// document.write(b + "<br>");
+
+// var c = 'Variables must begin with a letter,$ or_ . For example : $name, _name or name ';
+// document.write(c + "<br>");
+
+// var d = 'Variable names are case sensitive ';
+// document.write(d + "<br>");
+
+// var e = "Variable names should not be JS Keywords ";
+// document.write(e + "<br>");
+
+//              ==== Assignment No:5 ====
+
+
+// Q:1 
+
+// var plus1 = parseInt('3');
+// var plus2 = parseInt('5');
+// var plus3 = plus1 + plus2;
+// document.write('Sum of ' + plus1 + ' and ' + plus2 + ' is ' + plus3);
+
+// Q:2
+
+
+// var subtraction1 = parseInt('5');
+// var subtraction2 = parseInt('3');
+// var subtraction = subtraction1 - subtraction2;
+// document.write("<br><br> Subtraction " + subtraction);
+
+// var subtraction = subtraction1 * subtraction2;
+// document.write(' <br><br> Multiple ' + subtraction);
+
+// var subtraction = subtraction1 / subtraction2;
+// document.write('<br><br> Division ' + subtraction);
+
+// Q:3
+
+// var i;
+// i = 'after variable declaration is ';
+// document.write(" <br> " + i + '<br>');
+
+// var j = 'Initial value 5';
+// document.write(j);
+
+// var increment = 'Value after increment is: 6';
+// document.write("<br>" + increment);
+
+// var addition = 'Value after addition is: 13';
+// document.write('<br>' + addition)
+
+// var decrement = 'Value after decrement is: 12';
+// document.write('<br>' + decrement);
+
+// var di = 'The remainder is : 0';
+// document.write('<br>' + di);
+
+
+// Q:4
+
+// var cost = 600 / 5;
+// document.write('<br>Total cost to buy 5 tickets to a movie is ' + cost + 'PKR<br>');
+
+// Q:5
+
+// var table = 4;
+// document.write('Table of4')
+// for (var x = 1; x <= 10; x++) {
+
+//     document.write(table);
+//     document.write(' X ');
+//     document.write(x);
+//     document.write(' = ');
+//     document.write(table * x + '<br>');
+
+
+// }
+
+// Q:7
+
+// document.write('<h1> Shopping Cart </h1>')
+
+// var item1 = 'Price of item 1 is 650';
+// var quailty = 'Quantity of item 1 is 3';
+// var pritem = 'Price of item  2 is 100';
+// var itempri = 'Quantity of item 2 is 7';
+// var charges = 'shipping charges 100';
+
+// var itm = parseInt(650 * 3);
+// var pr = parseInt(100 * 7);
+// var cha = parseInt(100);
+// var total = itm + pr + cha;
+
+// document.write('<br><br>' + item1 + '<br>' + quailty + '<br>' + pritem + '<br>' + itempri + '<br>' + charges);
+// document.write('<br>Total cost of your order is ' + total);
+
+// // Q:8
+
+// document.write('<h1> Marks Sheet </h1>');
+
+// var ttalmks = parseInt(980);
+// var obtained = parseInt(804);
+// var Percentage = obtained / ttalmks * 100;
+
+
+// document.write('<br>Total marks: ' + ttalmks);
+// document.write('<br>Marks obtained: ' + obtained);
+// document.write('<br>Percentage: ' + Percentage);
+
+// Q:9
+
+// document.write('<h1> Currency in PKR </h1>')
+// var usdcurrency = parseInt(104.80 * 10);
+// var saudiriyal = parseInt(25 * 28);
+// var total = usdcurrency + saudiriyal;
+// document.write('<br><br>Total Currency in PKR: ' + total);
+
+// Q:10
+
+// var add = 5;
+// var plus = add + 30;
+// var multi = add * 10;
+// var Divi = add / 2;
+// document.write('<br><br>Miltiple ' + multi + '<br> Division ' + Divi + '<br> Plus ' + plus);
+
+// Q:11
+
+// var current = 2022;
+// var birth = 1996;
+// document.write('<h1> Age Calculator </h1>');
+// document.write('<br>Current Year: ' + current);
+// document.write('<br>Birth Year: ' + birth);
+// document.write('<br>Your Age: 24');
+
+
+// Q:13
+
+// document.write('<h1> The Lifetime Supply Calculator </h1>');
+// document.write('<br> favourite Snack: chocolate chip ');
+// document.write('<br> current age: 22');
+// document.write('<br> Estimated Maximum Age: 63');
+// document.write('<br> Amount of snack per day: 3');
+// document.write('<br> You will need 150 chocolate chip to last you until the ripe old age og 65');
